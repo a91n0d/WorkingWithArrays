729975d41c99c6c96ee833044ef5bb8e818a16ac
a91n0d <a91n0d@gmail.com>
2022-01-12 09:34:40 +0300

    Add project files.

---
+++ WorkingWithArrays/CreatingArray.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace WorkingWithArrays
+{
+    public static class CreatingArray
+    {
+        public static int[] CreateEmptyArrayOfIntegers()
+        {
+            return Array.Empty<int>();
+        }
+
+        public static bool[] CreateEmptyArrayOfBooleans()
+        {
+            return Array.Empty<bool>();
+        }
+
+        public static string[] CreateEmptyArrayOfStrings()
+        {
+            return Array.Empty<string>();
+        }
+
+        public static char[] CreateEmptyArrayOfCharacters()
+        {
+            return Array.Empty<char>();
+        }
+
+        public static double[] CreateEmptyArrayOfDoubles()
+        {
+            return Array.Empty<double>();
+        }
+
+        public static float[] CreateEmptyArrayOfFloats()
+        {
+            return Array.Empty<float>();
+        }
+
+        public static decimal[] CreateEmptyArrayOfDecimals()
+        {
+            return Array.Empty<decimal>();
+        }
+
+        public static int[] CreateArrayOfTenIntegersWithDefaultValues()
+        {
+            return new int[10];
+        }
+
+        public static bool[] CreateArrayOfTwentyBooleansWithDefaultValues()
+        {
+            return new bool[20];
+        }
+
+        public static string[] CreateArrayOfFiveEmptyStrings()
+        {
+            return new string[5];
+        }
+
+        public static char[] CreateArrayOfFifteenCharactersWithDefaultValues()
+        {
+            return new char[15];
+        }
+
+        public static double[] CreateArrayOfEighteenDoublesWithDefaultValues()
+        {
+            return new double[18];
+        }
+
+        public static float[] CreateArrayOfOneHundredFloatsWithDefaultValues()
+        {
+            return new float[100];
+        }
+
+        public static decimal[] CreateArrayOfOneThousandDecimalsWithDefaultValues()
+        {
+            return new decimal[1000];
+        }
+
+        public static int[] CreateIntArrayWithOneElement()
+        {
+            return new int[] { 123456 };
+        }
+
+        public static int[] CreateIntArrayWithTwoElements()
+        {
+            return new int[] { 1111111, 9999999 };
+        }
+
+        public static int[] CreateIntArrayWithTenElements()
+        {
+            return new int[] { 0, 4234, 3845, 2942, 1104, 9794, 923943, 7537, 4162, 10134 };
+        }
+
+        public static bool[] CreateBoolArrayWithOneElement()
+        {
+            return new bool[] { true };
+        }
+
+        public static bool[] CreateBoolArrayWithFiveElements()
+        {
+            return new bool[] { true, false, true, false, true };
+        }
+
+        public static bool[] CreateBoolArrayWithSevenElements()
+        {
+            return new bool[] { false, true, true, false, true, true, false };
+        }
+
+        public static string[] CreateStringArrayWithOneElement()
+        {
+            return new string[] { "one" };
+        }
+
+        public static string[] CreateStringArrayWithThreeElements()
+        {
+            return new string[] { "one", "two", "three" };
+        }
+
+        public static string[] CreateStringArrayWithSixElements()
+        {
+            return new string[] { "one", "two", "three", "four", "five", "six" };
+        }
+
+        public static char[] CreateCharArrayWithOneElement()
+        {
+            return new char[] { 'a' };
+        }
+
+        public static char[] CreateCharArrayWithThreeElements()
+        {
+            return new char[] { 'a', 'b', 'c' };
+        }
+
+        public static char[] CreateCharArrayWithNineElements()
+        {
+            return new char[] { '\u0061', '\u0062', '\u0063', '\u0064', '\u0065', '\u0066', '\u0067', '\u0068', '\u0061' };
+        }
+
+        public static double[] CreateDoubleArrayWithOneElement()
+        {
+            return new double[] { 1.12 };
+        }
+
+        public static double[] CreateDoubleWithFiveElements()
+        {
+            return new double[] { 1.12, 2.23, 3.34, 4.45, 5.56 };
+        }
+
+        public static double[] CreateDoubleWithNineElements()
+        {
+            return new double[] { 1.12, 2.23, 3.34, 4.45, 5.56, 6.67, 7.78, 8.89, 9.91 };
+        }
+
+        public static float[] CreateFloatArrayWithOneElement()
+        {
+            return new float[] { 123456789.123456f };
+        }
+
+        public static float[] CreateFloatWithThreeElements()
+        {
+            return new float[] { 1000000.123456f, 2223334444.123456f, 9999.999999f };
+        }
+
+        public static float[] CreateFloatWithFiveElements()
+        {
+            return new float[] { 1.0123f, 20.012345f, 300.01234567f, 4000.01234567f, 500000.01234567f };
+        }
+
+        public static decimal[] CreateDecimalArrayWithOneElement()
+        {
+            return new decimal[] { 10000.123456m };
+        }
+
+        public static decimal[] CreateDecimalWithFiveElements()
+        {
+            return new decimal[] { 1000.1234m, 100000.2345m, 100000.3456m, 1000000.456789m, 10000000.5678901m };
+        }
+
+        public static decimal[] CreateDecimalWithNineElements()
+        {
+            return new decimal[] { 10.122112m, 200.233223m, 3000.344334m, 40000.455445m, 500000.566556m, 6000000.677667m, 70000000.788778m, 800000000.899889m, 9000000000.911991m };
+        }
+    }
+}
